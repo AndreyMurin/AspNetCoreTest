91f1bf25449ee49a48a2be6ac84b2a8d9588ff61
AndreyMurin <96467840@mail.ru>
2016-10-27 16:31:19 +0300

    first commit

---
+++ src/AspNetCoreTest.Data.Abstractions/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTest.Data.Abstractions
+{
+    public interface IRepository
+    {
+        void SetStorageContext(IStorageContext storageContext);
+    }
+}
