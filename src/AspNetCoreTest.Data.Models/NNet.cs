9ebcf34150c0051c4102825e5666546283c115e6
AndreyMurin <96467840@mail.ru>
2016-12-02 16:52:02 +0300

    begin neuron test

---
+++ src/AspNetCoreTest.Data.Models/NNet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTest.Data.Models
+{
+    public class NNet
+    {
+        private List<Neuron> Neurons;
+
+        NNet(int size) {
+            Neurons = new List<Neuron>(size);
+        }
+    }
+}
---
+++ src/AspNetCoreTest.Data.Models/Neuron.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTest.Data.Models
+{
+    public class NInp
+    {
+        public decimal weight;
+        public Neuron neuron;
+    }
+
+    public class Neuron
+    {
+
+        public List<NInp> Input = new List<NInp>();
+
+        public long state;
+
+        void init(List<Neuron> inp)
+        {
+
+        }
+
+        void tick()
+        {
+
+        }
+
+
+    }
+}
